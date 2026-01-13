6d4624c0621fa00278ff3adc489614eb5acccdcb
YotamOphir <yotam4ophir@gmail.com>
2026-01-13 18:58:41 +0200

    Update HotelRoom.java

--- HotelRoom.java
+++ HotelRoom.java
@@ -1,33 +1,95 @@
-
-/**
- * Write a description of class HotelRoom here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class HotelRoom
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class HotelRoom
-     */
-    public HotelRoom()
-    {
-        // initialise instance variables
-        x = 0;
+    private int _roomNum;
+    private int _numBeds;
+    private boolean _occupied;
+    private String _guest;
+    
+    private static final String DEF_GUEST = "";
+    private static final int DEF_ROOM_NUM = 999;
+    private static final int DEF_NUM_BEDS = 2;
+    
+    public HotelRoom() {
+        setRoomNum(DEF_ROOM_NUM);
+        setNumBeds(DEF_NUM_BEDS);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public HotelRoom(int roomNum, int numBeds) {
+        setRoomNum(roomNum);
+        setNumBeds(numBeds);
+        _occupied = false;
+        _guest = DEF_GUEST;
+    }
+
+    public int getRoomNum() {
+        return _roomNum;
+    }
+    
+    public int getNumBeds() {
+        return _numBeds;
+    }
+    
+    public boolean isOccupied() {
+        return _occupied;
+    }
+    
+    public String getGuest() {
+        return _guest;
+    }
+    
+    public void setRoomNum(int roomNum) {
+        if (roomNumOk(roomNum)) {
+            _roomNum = roomNum;
+        }
+    }
+    
+    public void setNumBeds(int numBeds) {
+        if (numBedsOk(numBeds)) {
+            _numBeds = numBeds;
+        }
+    }
+    
+    private boolean roomNumOk(int roomNum) {
+        return roomNum > 99 && roomNum < 1000;
+    }
+    
+    private boolean numBedsOk(int numbed) {
+        return numbed >= 2 && numbed <= 4;
+    }
+    
+    public String toString() {
+        if (_occupied) {
+            return "Room " + _roomNum + ", "  + _numBeds + " Beds: Occupied by " + _guest;
+        } else {
+            return "Room " + _roomNum + ", "  + _numBeds + " Beds: Available";
+        }
+    }
+    
+    public boolean equals(HotelRoom other) {
+        return _roomNum == other._roomNum && _numBeds == other._numBeds;
+    }
+    
+    public boolean before(HotelRoom other) {
+        return _roomNum < other._roomNum;
+    }
+    
+    public boolean after(HotelRoom other) {
+        return other.before(this);
+    }
+    
+    public boolean checkIn(String guest) {
+        if (_occupied) { 
+            return false;
+        } else {
+            _occupied = true;
+            _guest = guest;
+            return true;
+        }
+    }
+    
+    public void checkOut() {
+        _occupied = false;
+        _guest = DEF_GUEST;
     }
 }
