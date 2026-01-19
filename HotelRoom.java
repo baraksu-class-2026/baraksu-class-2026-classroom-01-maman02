849242d5e8198e308c1f4af126cd70662a3a1593
YotamOphir <yotam4ophir@gmail.com>
2026-01-19 20:01:43 +0200

    Update HotelRoom.java

--- HotelRoom.java
+++ HotelRoom.java
@@ -77,6 +77,9 @@ public class HotelRoom {
     }
     
     public boolean equals(HotelRoom other) {
+        if (other == null) {
+            return false;
+        }
         return _roomNum == other._roomNum && _numBeds == other._numBeds;
     }
     
